da6d4ef90e554f267ec75f1add8f701a50537069
vornixfunding-design <vornixfunding@gmail.com>
2026-02-21 02:00:22 +0530

    Remove OTP/Resend backend implementation

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ import { getHealth } from '../controllers/healthController.js';
 import ctraderRoutes from './ctraderRoutes.js';
 import authRoutes from './authRoutes.js';
 import planRoutes from './planRoutes.js';
-import otpRoutes from './otpRoutes.js';
 
 const router = Router();
 
@@ -11,6 +10,5 @@ router.get('/health', getHealth);
 router.use('/ctrader', ctraderRoutes);
 router.use('/auth', authRoutes);
 router.use('/plans', planRoutes);
-router.use('/otp', otpRoutes);
 
 export default router;
