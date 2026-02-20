a91787c12cdcbd0607460f8c36ef5e79956d311d
vornixfunding-design <vornixfunding@gmail.com>
2026-02-21 00:52:12 +0530

    Update email.js

--- src/utils/email.js
+++ src/utils/email.js
@@ -2,7 +2,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function sendOTP(to, otp) {
+export async function sendEmail(to, otp) {
   try {
     await resend.emails.send({
       from: "Vornix <no-reply@vornixfunding.com>",
